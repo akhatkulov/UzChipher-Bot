a95a8841ff50e508a44bee62871db8b2f792fd67
akhatkulov <amsmetafra@gmail.com>
2023-12-25 20:14:52 +0500

    version 1

---
+++ app.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("YOUR_BOT_TOKEN",parse_mode='html')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "<b> Howdy, how are you doing? <b> ")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
