b27b8fc449d6eaeaeb3b47d268ee4575cff986c2
akhatkulov <amsmetafra@gmail.com>
2023-12-29 11:42:13 +0500

    lang change problem

--- helper/data_config.py
+++ helper/data_config.py
@@ -9,12 +9,14 @@ db.execute("""CREATE TABLE IF NOT EXISTS users(
     WORK1 TEXT DEFAULT "none",
     WOKR2 TEXT DEFAULT "none",
     STEP TEXT DEFAULT "home")""")
-db.execute("CREATE TABLE IF NOT EXISTS stat(ID INT UNIQUE DEFAULT 1, WORK INT DEFAULT 1)")
+db.execute("CREATE TABLE IF NOT EXISTS stat(ID INTIGER UNIQUE DEFAULT 1, WORK INTIGER DEFAULT 1)")
 
 def pluser():
-    x = db.cursor("SELECT work FROM stat WHERE id=1")
-    x+=1
-    db.execute("UPDATE stat SET work=? WHERE id=1",(x))
+    # x = db.execute("SELECT * FROM stat WHERE id=1")
+    # x=int()
+    # x+=1
+    # db.execute("UPDATE stat SET work=? WHERE id=1",(x))
+    print("--[]--")
 
 def add_step(cid,step):
     db.execute(f"UPDATE users SET step=? WHERE cid=?",(str(step),int(cid)))
